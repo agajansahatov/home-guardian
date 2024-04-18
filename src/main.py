daccc562c1e17abf2d3e7c0d8e89698e09d9201d
Agajan Sahatov <agajan.st@gmail.com>
2024-04-18 08:39:05 +0800

    Update lightbulb

--- src/main.py
+++ src/main.py
@@ -1,15 +1,53 @@
 import time
-
 from pymata4EX import pymata4EX
+from modules.climate_sensor import ClimateSensor
+from modules.display import Display
+from modules.fan import Fan
 from modules.fire_alarm import FireAlarm
+from modules.light_bulb import LightBulb
+from modules.slide_potentiometer import SlidePotentiometer
 
 if __name__ == "__main__":
+    # Initialize board and components
     board = pymata4EX.Pymata4EX()
-
+    climate_sensor = ClimateSensor(board)
+    display = Display(board)
+    fan = Fan(board)
     fire_alarm = FireAlarm(board)
+    light_bulb = LightBulb(board)
+    slide_potentiometer = SlidePotentiometer(board)
 
-    fire_alarm.start()
+    display.show('Activating', 'sensors...')
 
-    time.sleep(5)
+    try:
+        while True:
+            # Read temperature from climate sensor
+            temperature = climate_sensor.read_temperature()
 
-    fire_alarm.stop()
+            # Update display with temperature
+            display.show(str(temperature))
+
+            # Check temperature and control components accordingly
+            if temperature < 15:
+                fan.turn_off()
+                light_bulb.set_color("blue")
+            elif 15 <= temperature <= 30:
+                fan.turn_off()
+                light_bulb.set_color("green")
+            elif 30 < temperature <= 50:
+                fan.turn_on()
+                light_bulb.set_color("red")
+            else:
+                while true:
+                    light_bulb.set_color
+                    time.sleep(1)
+                fire_alarm.start()
+
+            # Check slide potentiometer for user input
+
+            # Add a delay to avoid excessive polling
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        # Clean up GPIO pins on keyboard interrupt
+        board.shutdown()
--- src/modules/lightbulb.py
+++ src/modules/lightbulb.py
@@ -8,17 +8,20 @@ class LightBulb:
         self.board.set_pin_mode_pwm_output(self.green_pin)
         self.board.set_pin_mode_pwm_output(self.blue_pin)
         self.is_on = False
+        self.color = "white"
 
     def turn_on(self):
         self.board.pwm_write(self.red_pin, 255)
         self.board.pwm_write(self.green_pin, 255)
         self.board.pwm_write(self.blue_pin, 255)
+        self.color = "black"
         self.is_on = True
 
     def turn_off(self):
         self.board.pwm_write(self.red_pin, 0)
         self.board.pwm_write(self.green_pin, 0)
         self.board.pwm_write(self.blue_pin, 0)
+        self.color = "white"
         self.is_on = False
 
     def set_color(self, color):
@@ -26,17 +29,25 @@ class LightBulb:
             self.board.pwm_write(self.red_pin, 255)
             self.board.pwm_write(self.green_pin, 0)
             self.board.pwm_write(self.blue_pin, 0)
+            self.color = "red"
         elif color == "green":
             self.board.pwm_write(self.red_pin, 0)
             self.board.pwm_write(self.green_pin, 255)
             self.board.pwm_write(self.blue_pin, 0)
+            self.color = "green"
         elif color == "blue":
             self.board.pwm_write(self.red_pin, 0)
             self.board.pwm_write(self.green_pin, 0)
             self.board.pwm_write(self.blue_pin, 255)
+            self.color = "blue"
         elif color == "yellow":
             self.board.pwm_write(self.red_pin, 255)
             self.board.pwm_write(self.green_pin, 255)
             self.board.pwm_write(self.blue_pin, 0)
+            self.color = "yellow"
         else:
             self.turn_off()
+            self.color = "white"
+
+    def get_color(self):
+        return self.color
